39d1514d34f5f9ffdd8ddcdfe6bb3d6c76cf74a5
ghabriel <rihulfaakbar@gmail.com>
2025-02-24 15:51:41 +0700

    update PaginationViewServiceProvider.php

--- src/PaginationViewServiceProvider.php
+++ src/PaginationViewServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Ghabriel\PaginationView;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
 class PaginationViewServiceProvider extends ServiceProvider
@@ -9,13 +10,22 @@ class PaginationViewServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
+            // Set the target path
             $target = resource_path('views/vendor/pagination');
 
-            $views = [
-                'fomanticui' => __DIR__.'/../resources/views/fomanticui',
-                'bulma' => __DIR__.'/../resources/views/bulma',
-                'bootstrap' => __DIR__.'/../resources/views/bootstrap',
-            ];
+            // Get views path
+            $directory = __DIR__.'/../resources/views';
+
+            // Get all folders inside views
+            $folders = File::directories($directory);
+
+            $views = [];
+
+            // Create associative array for all vendors, vendor_name => folder_path
+            foreach ($folders as $folder) {
+                $folderName = basename($folder);
+                $views[$folderName] = "$directory/$folderName";
+            }
 
             // Create an array where the original view paths are keys and the target path is the value
             $publishPaths = array_fill_keys(
