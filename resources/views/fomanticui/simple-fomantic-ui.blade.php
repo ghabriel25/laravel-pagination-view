de16f06fdac53a68e6714abe6d580e9f11908a95
ghabriel <rihulfaakbar@gmail.com>
2025-02-25 04:47:26 +0700

    update simple-fomantic-ui.blade.php

--- resources/views/fomanticui/simple-fomantic-ui.blade.php
+++ resources/views/fomanticui/simple-fomantic-ui.blade.php
@@ -1,33 +1,35 @@
 @if ($paginator->hasPages())
     <div class="ui equal width grid">
-        {{-- Previous Page Link --}}
-        <div class="left floated column">
-            @if ($paginator->onFirstPage())
-                <div class="ui disabled button" aria-disabled="true">
-                    {!! __('pagination.previous') !!}
-                </div>
-            @else
-                <a class="ui button"
-                   href="{{ $paginator->previousPageUrl() }}"
-                   rel="prev">
-                    {!! __('pagination.previous') !!}
-                </a>
-            @endif
-        </div>
+        <div class="row">
+            {{-- Previous Page Link --}}
+            <div class="left floated column">
+                @if ($paginator->onFirstPage())
+                    <div class="ui disabled button" aria-disabled="true">
+                        {!! __('pagination.previous') !!}
+                    </div>
+                @else
+                    <a class="ui button"
+                       href="{{ $paginator->previousPageUrl() }}"
+                       rel="prev">
+                        {!! __('pagination.previous') !!}
+                    </a>
+                @endif
+            </div>
 
-        {{-- Next Page Link --}}
-        <div class="right floated right aligned column">
-            @if ($paginator->hasMorePages())
-                <a class="ui button"
-                   href="{{ $paginator->nextPageUrl() }}"
-                   rel="next">
-                    {!! __('pagination.next') !!}
-                </a>
-            @else
-                <div class="ui disabled button" aria-disabled="true">
-                    {!! __('pagination.next') !!}
-                </div>
-            @endif
+            {{-- Next Page Link --}}
+            <div class="right floated right aligned column">
+                @if ($paginator->hasMorePages())
+                    <a class="ui button"
+                       href="{{ $paginator->nextPageUrl() }}"
+                       rel="next">
+                        {!! __('pagination.next') !!}
+                    </a>
+                @else
+                    <div class="ui disabled button" aria-disabled="true">
+                        {!! __('pagination.next') !!}
+                    </div>
+                @endif
+            </div>
         </div>
     </div>
 @endif
