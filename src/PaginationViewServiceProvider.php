0728effc89845a9295fecb419ad48d37de7136cc
ghabriel <rihulfaakbar@gmail.com>
2025-02-27 10:33:01 +0700

    add autoload views with namespace

--- src/PaginationViewServiceProvider.php
+++ src/PaginationViewServiceProvider.php
@@ -9,44 +9,55 @@ class PaginationViewServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        // Get views path
+        $directory = __DIR__ . '/../resources/views';
+
+        // Get all folders inside views
+        $folders = File::directories($directory);
+
+        $views = [];
+
+        // Create associative array for all vendors, vendor_name => folder_path
+        foreach ($folders as $folder) {
+            $vendorName = basename($folder);
+            $source = $directory . DIRECTORY_SEPARATOR . $vendorName;
+
+            if (is_dir($source)) {
+                $views[$vendorName] = $source;
+            }
+        }
+
+        // Load all views with namespace
+        $this->loadViewsFrom(
+            array_values($views),
+            'pagination'
+        );
+
         if ($this->app->runningInConsole()) {
-            // Set the target path
-            $target = $this->app->resourcePath('views/vendor/pagination');
+            $this->bootInConsole($views);
+        }
+    }
 
-            // Get views path
-            $directory = __DIR__ . '/../resources/views';
+    protected function bootInConsole($views): void
+    {
+        // Set the target path
+        $target = $this->app->resourcePath('views/vendor/pagination');
 
-            // Get all folders inside views
-            $folders = File::directories($directory);
+        // Create an array where the original view paths are keys and the target path is the value
+        $publishPaths = array_fill_keys(
+            array_values($views),
+            $target
+        );
 
-            $views = [];
+        // Publish all views under a single tag
+        $this->publishes($publishPaths, 'pagination-view-all');
 
-            // Create associative array for all vendors, vendor_name => folder_path
-            foreach ($folders as $folder) {
-                $vendorName = basename($folder);
-                $source = $directory . DIRECTORY_SEPARATOR . $vendorName;
-
-                if (is_dir($source)) {
-                    $views[$vendorName] = $source;
-                }
-            }
-
-            // Create an array where the original view paths are keys and the target path is the value
-            $publishPaths = array_fill_keys(
-                array_values($views),
-                $target
+        // Publish individual views under specific tags
+        foreach ($views as $vendor => $path) {
+            $this->publishes(
+                [$path => $target],
+                "pagination-view-$vendor"
             );
-
-            // Publish all views under a single tag
-            $this->publishes($publishPaths, 'pagination-view-all');
-
-            // Publish individual views under specific tags
-            foreach ($views as $vendor => $path) {
-                $this->publishes(
-                    [$path => $target],
-                    "pagination-view-$vendor"
-                );
-            }
         }
     }
 }
