2ace9e7b0856b993e516cc08627124dbab0104dd
ghabriel <rihulfaakbar@gmail.com>
2025-03-03 21:09:49 +0700

    feat(structure): update cirrus.blade.php

--- resources/views/cirrus/cirrus.blade.php
+++ resources/views/cirrus/cirrus.blade.php
@@ -1,46 +1,48 @@
 @if ($paginator->hasPages())
-    <div class="btn-group">
-        {{-- Previous Page Link --}}
-        @if ($paginator->onFirstPage())
-            <button class="btn--disabled" aria-disabled="true">
-                {!! __('pagination.previous') !!}
-            </button>
-        @else
-            <a class="btn" href="{{ $paginator->previousPageUrl() }}">
-                {!! __('pagination.previous') !!}
-            </a>
-        @endif
+    {{-- Previous Page Link --}}
+    @if ($paginator->onFirstPage())
+        <button class="btn btn--sm btn--disabled mr-1" aria-disabled="true">
+            {!! __('pagination.previous') !!}
+        </button>
+    @else
+        <a class="btn btn--sm mr-1" href="{{ $paginator->previousPageUrl() }}">
+            {!! __('pagination.previous') !!}
+        </a>
+    @endif
 
-        {{-- Pagination Elements --}}
-        @foreach ($elements as $element)
-            {{-- "Three Dots" Separator --}}
-            @if (is_string($element))
-                <button class="btn--disabled" aria-disabled="true">{{ $element }}</button>
-            @endif
-
-            {{-- Array Of Links --}}
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <button class="btn-link">{{ $page }}</button>
-                    @else
-                        <a class="btn" href="{{ $url }}">
-                            {{ $page }}
-                        </a>
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
-
-        {{-- Next Page Link --}}
-        @if ($paginator->hasMorePages())
-            <a class="btn" href="{{ $paginator->nextPageUrl() }}">
-                {!! __('pagination.next') !!}
-            </a>
-        @else
-            <button class="btn--disabled" aria-disabled="true">
-                {!! __('pagination.next') !!}
+    {{-- Pagination Elements --}}
+    @foreach ($elements as $element)
+        {{-- "Three Dots" Separator --}}
+        @if (is_string($element))
+            <button class="btn btn--sm btn--disabled mr-1" aria-disabled="true">
+                {{ $element }}
             </button>
         @endif
-    </div>
+
+        {{-- Array Of Links --}}
+        @if (is_array($element))
+            @foreach ($element as $page => $url)
+                @if ($page == $paginator->currentPage())
+                    <button class="btn btn--sm btn-link mr-1">
+                        {{ $page }}
+                    </button>
+                @else
+                    <a class="btn btn--sm mr-1" href="{{ $url }}">
+                        {{ $page }}
+                    </a>
+                @endif
+            @endforeach
+        @endif
+    @endforeach
+
+    {{-- Next Page Link --}}
+    @if ($paginator->hasMorePages())
+        <a class="btn btn--sm mr-1" href="{{ $paginator->nextPageUrl() }}">
+            {!! __('pagination.next') !!}
+        </a>
+    @else
+        <button class="btn btn--sm btn--disabled mr-1" aria-disabled="true">
+            {!! __('pagination.next') !!}
+        </button>
+    @endif
 @endif
