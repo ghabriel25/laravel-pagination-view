88de115e0d759010218cd52d2ec376bafee717e9
ghabriel <rihulfaakbar@gmail.com>
2025-03-03 13:25:35 +0700

    feat(structure): revert to original fomantic-ui.blade.php

--- resources/views/fomanticui/fomantic-ui.blade.php
+++ resources/views/fomanticui/fomantic-ui.blade.php
@@ -1,12 +1,12 @@
 @if ($paginator->hasPages())
-    <div class="ui compact wrapping spaced buttons" role="navigation">
+    <div class="ui pagination menu">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <div class="ui basic button disabled" aria-disabled="true">
+            <div class="disabled item" aria-disabled="true">
                 {!! __('pagination.previous') !!}
             </div>
         @else
-            <a class="ui basic button"
+            <a class="item"
                href="{{ $paginator->previousPageUrl() }}"
                rel="prev">
                 {!! __('pagination.previous') !!}
@@ -17,22 +17,16 @@
         @foreach ($elements as $element)
             {{-- "Three Dots" Separator --}}
             @if (is_string($element))
-                <div class="ui basic button disabled" aria-disabled="true">
-                    {{ $element }}
-                </div>
+                <div class="disabled item" aria-disabled="true">{{ $element }}</div>
             @endif
 
             {{-- Array Of Links --}}
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <div class="ui active button" aria-current="page">
-                            {{ $page }}
-                        </div>
+                        <div class="active item" aria-current="page">{{ $page }}</div>
                     @else
-                        <a class="ui basic button" href="{{ $url }}">
-                            {{ $page }}
-                        </a>
+                        <a class="item" href="{{ $url }}">{{ $page }}</a>
                     @endif
                 @endforeach
             @endif
@@ -40,13 +34,13 @@
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a class="ui basic button"
+            <a class="item"
                href="{{ $paginator->nextPageUrl() }}"
                rel="next">
                 {!! __('pagination.next') !!}
             </a>
         @else
-            <div class="ui basic button disabled" aria-disabled="true">
+            <div class="disabled item" aria-disabled="true">
                 {!! __('pagination.next') !!}
             </div>
         @endif
