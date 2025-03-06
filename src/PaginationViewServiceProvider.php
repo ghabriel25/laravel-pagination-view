5e48a91e0e3fe528427bc4d7d53eeb5a050788a7
Rihulfa Akbar <151924216+ghabriel25@users.noreply.github.com>
2025-03-06 09:15:35 +0700

    Update PaginationViewServiceProvider.php

--- src/PaginationViewServiceProvider.php
+++ src/PaginationViewServiceProvider.php
@@ -7,55 +7,58 @@ use Illuminate\Support\ServiceProvider;
 
 class PaginationViewServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        // Register any application services.
+    }
+
     public function boot(): void
     {
-        //  Get the directory
-        $directory = __DIR__ . '/../resources/views';
+        $views = $this->getViewFolders(__DIR__ . '/../resources/views');
+        $this->loadViewsFrom(array_values($views), 'pagination');
 
-        // Get all folders
+        if ($this->app->runningInConsole()) {
+            $this->publishViews($views);
+        }
+    }
+
+    /**
+     * Get all view folders.
+     *
+     * @param string $directory
+     * @return array
+     */
+    private function getViewFolders(string $directory): array
+    {
         $folders = File::directories($directory);
-
         $views = [];
 
-        // Create associative array for all vendors, vendor_name => folder_path
         foreach ($folders as $folder) {
             if (is_dir($folder)) {
                 $views[basename($folder)] = $folder;
             }
         }
 
-        // Load all views with namespace
-        $this->loadViewsFrom(
-            array_values($views),
-            'pagination'
-        );
-
-        // Boot in console
-        if ($this->app->runningInConsole()) {
-            $this->bootInConsole($views);
-        }
+        return $views;
     }
 
-    protected function bootInConsole($views): void
+    /**
+     * Publish views for console.
+     *
+     * @param array $views
+     * @return void
+     */
+    private function publishViews(array $views): void
     {
-        // Get the target path
         $target = $this->app->resourcePath('views/vendor/pagination');
-
-        // Create an array where the original view paths are keys and the target path is the value
-        $publishPaths = array_fill_keys(
-            array_values($views),
-            $target
-        );
+        $publishPaths = array_fill_keys(array_values($views), $target);
 
         // Publish all views under a single tag
         $this->publishes($publishPaths, 'pagination-view-all');
 
         // Publish individual views under specific tags
         foreach ($views as $vendor => $path) {
-            $this->publishes(
-                [$path => $target],
-                "pagination-view-$vendor"
-            );
+            $this->publishes([$path => $target], "pagination-view-$vendor");
         }
     }
 }
