09245170845bc4495fd6eeb5f819b4b5304a76b3
ghabriel <rihulfaakbar@gmail.com>
2025-03-09 23:40:08 +0700

    feat(structure): update responsiveness of uikit.blade.php

--- resources/views/uikit/uikit.blade.php
+++ resources/views/uikit/uikit.blade.php
@@ -1,26 +1,38 @@
 @if ($paginator->hasPages())
     @props(['darkMode' => false])
 
-    <div class="uk-flex uk-flex-wrap @if ($darkMode) uk-light @endif">
+    <div class="uk-flex uk-flex-between@m uk-flex-left uk-flex-wrap @if ($darkMode) uk-light @endif">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <button class="uk-button uk-button-small uk-button-default uk-width-auto@s uk-width-1-1 uk-margin-xsmall-right@s uk-margin-small-top uk-margin-xsmall-right"
+            <button class="uk-button uk-button-small uk-button-default uk-width-auto@m uk-width-1-2 uk-margin-small-top"
                     disabled>
                 {!! __('pagination.previous') !!}
             </button>
         @else
-            <a class="uk-button uk-button-small uk-button-default uk-width-auto@s uk-width-1-1 uk-margin-xsmall-right@s uk-margin-small-top uk-margin-xsmall-right"
+            <a class="uk-button uk-button-small uk-button-default uk-width-auto@m uk-width-1-2 uk-margin-small-top"
                href="{{ $paginator->previousPageUrl() }}">
                 {!! __('pagination.previous') !!}
             </a>
+        @endif
+
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <a class="uk-button uk-button-small uk-button-default uk-width-auto@m uk-width-1-2 uk-flex-last@m uk-margin-small-top"
+               href="{{ $paginator->nextPageUrl() }}">
+                {!! __('pagination.next') !!}
+            </a>
+        @else
+            <button class="uk-button uk-button-small uk-button-default uk-width-auto@m uk-width-1-2 uk-flex-last@m uk-margin-small-top"
+                    disabled>
+                {!! __('pagination.next') !!}
+            </button>
         @endif
 
         {{-- Pagination Elements --}}
         @foreach ($elements as $element)
             {{-- "Three Dots" Separator --}}
             @if (is_string($element))
-                <button class="uk-button uk-button-small uk-button-default uk-margin-xsmall-right uk-margin-small-top"
-                        disabled>
+                <button class="uk-button uk-button-small uk-button-default uk-margin-small-top" disabled>
                     {{ $element }}
                 </button>
             @endif
@@ -29,12 +41,11 @@
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <button
-                                class="uk-button uk-button-small uk-button-primary uk-margin-xsmall-right uk-margin-small-top">
+                        <button class="uk-button uk-button-small uk-button-primary uk-margin-small-top">
                             {{ $page }}
                         </button>
                     @else
-                        <a class="uk-button uk-button-small uk-button-default uk-margin-xsmall-right uk-margin-small-top"
+                        <a class="uk-button uk-button-small uk-button-default uk-margin-small-top"
                            href="{{ $url }}">
                             {{ $page }}
                         </a>
@@ -42,18 +53,5 @@
                 @endforeach
             @endif
         @endforeach
-
-        {{-- Next Page Link --}}
-        @if ($paginator->hasMorePages())
-            <a class="uk-button uk-button-small uk-button-default uk-width-auto@s uk-width-1-1 uk-margin-xsmall-right@s uk-margin-small-top"
-               href="{{ $paginator->nextPageUrl() }}">
-                {!! __('pagination.next') !!}
-            </a>
-        @else
-            <button class="uk-button uk-button-small uk-button-default uk-width-auto@s uk-width-1-1 uk-margin-xsmall-right@s uk-margin-small-top"
-                    disabled>
-                {!! __('pagination.next') !!}
-            </button>
-        @endif
     </div>
 @endif
