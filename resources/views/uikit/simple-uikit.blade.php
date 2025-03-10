d3b96489103d7efa5d2ed04ac2ab209be014641c
ghabriel <rihulfaakbar@gmail.com>
2025-03-10 21:03:17 +0700

    feat(structure): update uikit responsiveness

--- resources/views/uikit/simple-uikit.blade.php
+++ resources/views/uikit/simple-uikit.blade.php
@@ -4,12 +4,12 @@
     <div class="uk-flex uk-flex-between @if ($darkMode) uk-light @endif">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <button class="uk-button uk-button-small uk-button-default uk-margin-small-top uk-margin-small-right"
+            <button class="uk-button uk-button-small uk-button-default uk-width-1-2 uk-width-1-5@s uk-width-1-6@l uk-margin-small-top"
                     disabled>
                 {!! __('pagination.previous') !!}
             </button>
         @else
-            <a class="uk-button uk-button-small uk-button-default uk-margin-small-top uk-margin-small-right"
+            <a class="uk-button uk-button-small uk-button-default uk-width-1-2 uk-width-1-5@s uk-width-1-6@l uk-margin-small-top"
                href="{{ $paginator->previousPageUrl() }}">
                 {!! __('pagination.previous') !!}
             </a>
@@ -17,12 +17,12 @@
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a class="uk-button uk-button-small uk-button-default uk-margin-small-top uk-margin-small-right"
+            <a class="uk-button uk-button-small uk-button-default uk-width-1-2 uk-width-1-5@s uk-width-1-6@l uk-margin-small-top"
                href="{{ $paginator->nextPageUrl() }}">
                 {!! __('pagination.next') !!}
             </a>
         @else
-            <button class="uk-button uk-button-small uk-button-default uk-margin-small-top uk-margin-small-right"
+            <button class="uk-button uk-button-small uk-button-default uk-width-1-2 uk-width-1-5@s uk-width-1-6@l uk-margin-small-top"
                     disabled>
                 {!! __('pagination.next') !!}
             </button>
