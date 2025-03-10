be9ae41857336f61c591afee57135991b104430e
ghabriel <rihulfaakbar@gmail.com>
2025-03-10 23:23:49 +0700

    feat(structure): update cirrus ui responsiveness

--- resources/views/cirrus/cirrus.blade.php
+++ resources/views/cirrus/cirrus.blade.php
@@ -1,57 +1,74 @@
 @if ($paginator->hasPages())
     @props(['darkMode' => false])
 
-    {{-- Previous Page Link --}}
-    @if ($paginator->onFirstPage())
-        <button class="btn--sm btn--disabled mr-1 @if ($darkMode) bg-dark text-white @endif"
-                aria-disabled="true">
-            {!! __('pagination.previous') !!}
-        </button>
-    @else
-        <a class="btn btn--sm mr-1 @if ($darkMode) bg-dark text-white @endif"
-           href="{{ $paginator->previousPageUrl() }}">
-            {!! __('pagination.previous') !!}
-        </a>
-    @endif
-
-    {{-- Pagination Elements --}}
-    @foreach ($elements as $element)
-        {{-- "Three Dots" Separator --}}
-        @if (is_string($element))
-            <button class="btn--sm btn--disabled mr-1 @if ($darkMode) bg-dark text-white @endif"
+    <div class="u-flex u-justify-space-between u-flex-wrap">
+        {{-- Previous Page Link --}}
+        @if ($paginator->onFirstPage())
+            <button class="u-block btn--sm btn--disabled md:w-auto w-16 @if ($darkMode) bg-dark text-white @endif"
                     aria-disabled="true">
-                {{ $element }}
+                {!! __('pagination.previous') !!}
+            </button>
+        @else
+            <a class="u-block btn btn--sm md:w-auto w-16 @if ($darkMode) bg-dark text-white @endif"
+               href="{{ $paginator->previousPageUrl() }}">
+                {!! __('pagination.previous') !!}
+            </a>
+        @endif
+
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <a class="u-block md:u-none btn btn--sm w-16 @if ($darkMode) bg-dark text-white @endif"
+               href="{{ $paginator->nextPageUrl() }}">
+                {!! __('pagination.next') !!}
+            </a>
+        @else
+            <button class="u-block md:u-none btn--sm btn--disabled w-16 @if ($darkMode) bg-dark text-white @endif"
+                    aria-disabled="true">
+                {!! __('pagination.next') !!}
             </button>
         @endif
 
-        {{-- Array Of Links --}}
-        @if (is_array($element))
-            @foreach ($element as $page => $url)
-                @if ($page == $paginator->currentPage())
-                    <button
-                            class="btn--sm btn-link mr-1 @if ($darkMode) bg-link-dark text-white @endif">
-                        {{ $page }}
+        <div class="u-flex u-flex-wrap u-col-gap-1 md:u-col-gap-2">
+            {{-- Pagination Elements --}}
+            @foreach ($elements as $element)
+                {{-- "Three Dots" Separator --}}
+                @if (is_string($element))
+                    <button class="btn--sm btn--disabled @if ($darkMode) bg-dark text-white @endif"
+                            aria-disabled="true">
+                        {{ $element }}
                     </button>
-                @else
-                    <a class="btn btn--sm mr-1 @if ($darkMode) bg-dark text-white @endif"
-                       href="{{ $url }}">
-                        {{ $page }}
-                    </a>
+                @endif
+
+                {{-- Array Of Links --}}
+                @if (is_array($element))
+                    @foreach ($element as $page => $url)
+                        @if ($page == $paginator->currentPage())
+                            <button
+                                    class="btn--sm btn-link @if ($darkMode) bg-link-dark text-white @endif">
+                                {{ $page }}
+                            </button>
+                        @else
+                            <a class="btn btn--sm @if ($darkMode) bg-dark text-white @endif"
+                               href="{{ $url }}">
+                                {{ $page }}
+                            </a>
+                        @endif
+                    @endforeach
                 @endif
             @endforeach
-        @endif
-    @endforeach
+        </div>
 
-    {{-- Next Page Link --}}
-    @if ($paginator->hasMorePages())
-        <a class="btn btn--sm mr-1 @if ($darkMode) bg-dark text-white @endif"
-           href="{{ $paginator->nextPageUrl() }}">
-            {!! __('pagination.next') !!}
-        </a>
-    @else
-        <button class="btn--sm btn--disabled mr-1 @if ($darkMode) bg-dark text-white @endif"
-                aria-disabled="true">
-            {!! __('pagination.next') !!}
-        </button>
-    @endif
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <a class="u-none md:u-block btn btn--sm md:w-auto @if ($darkMode) bg-dark text-white @endif"
+               href="{{ $paginator->nextPageUrl() }}">
+                {!! __('pagination.next') !!}
+            </a>
+        @else
+            <button class="u-none md:u-block btn--sm btn--disabled md:w-auto @if ($darkMode) bg-dark text-white @endif"
+                    aria-disabled="true">
+                {!! __('pagination.next') !!}
+            </button>
+        @endif
+    </div>
 @endif
--- resources/views/cirrus/simple-cirrus.blade.php
+++ resources/views/cirrus/simple-cirrus.blade.php
@@ -4,12 +4,12 @@
     <div class="u-flex u-justify-space-between">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <button class="btn--sm btn--disabled @if ($darkMode) bg-dark text-white @endif"
+            <button class="u-block btn--sm btn--disabled w-16 @if ($darkMode) bg-dark text-white @endif"
                     aria-disabled="true">
                 {!! __('pagination.previous') !!}
             </button>
         @else
-            <a class="btn btn--sm @if ($darkMode) bg-dark text-white @endif"
+            <a class="u-block btn btn--sm w-16 @if ($darkMode) bg-dark text-white @endif"
                href="{{ $paginator->previousPageUrl() }}">
                 {!! __('pagination.previous') !!}
             </a>
@@ -17,12 +17,12 @@
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a class="btn btn--sm @if ($darkMode) bg-dark text-white @endif"
+            <a class="u-block btn btn--sm w-16 @if ($darkMode) bg-dark text-white @endif"
                href="{{ $paginator->nextPageUrl() }}">
                 {!! __('pagination.next') !!}
             </a>
         @else
-            <button class="btn--sm btn--disabled @if ($darkMode) bg-dark text-white @endif"
+            <button class="u-block btn--sm btn--disabled w-16 @if ($darkMode) bg-dark text-white @endif"
                     aria-disabled="true">
                 {!! __('pagination.next') !!}
             </button>
