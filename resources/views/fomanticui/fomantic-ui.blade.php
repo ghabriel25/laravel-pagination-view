fa02f92efe17960edb1637bb6b7e44b6496514bd
ghabriel <rihulfaakbar@gmail.com>
2025-03-11 13:03:17 +0700

    feat(structure): update fomantic-ui.blade.php responsiveness

--- resources/views/fomanticui/fomantic-ui.blade.php
+++ resources/views/fomanticui/fomantic-ui.blade.php
@@ -1,60 +1,103 @@
 @if ($paginator->hasPages())
     @props(['darkMode' => false])
 
-    <div class="ui compact wrapping spaced buttons">
+    <div class="ui three column compact center aligned grid">
         {{-- Previous Page Link --}}
-        @if ($paginator->onFirstPage())
-            <div class="ui disabled left floated @if ($darkMode) inverted basic @endif button"
-                 aria-disabled="true">
-                {!! __('pagination.previous') !!}
+        <div class="two wide computer four wide tablet eight wide mobile left floated column">
+            <div class="ui compact fluid buttons">
+                @if ($paginator->onFirstPage())
+                    <div class="ui disabled @if ($darkMode) inverted basic @endif button"
+                         aria-disabled="true">
+                        {!! __('pagination.previous') !!}
+                    </div>
+                @else
+                    <a class="ui @if ($darkMode) inverted @else basic @endif button"
+                       href="{{ $paginator->previousPageUrl() }}"
+                       rel="prev">
+                        {!! __('pagination.previous') !!}
+                    </a>
+                @endif
             </div>
-        @else
-            <a class="ui left floated @if ($darkMode) inverted @else basic @endif button"
-               href="{{ $paginator->previousPageUrl() }}"
-               rel="prev">
-                {!! __('pagination.previous') !!}
-            </a>
-        @endif
+        </div>
 
         {{-- Pagination Elements --}}
-        @foreach ($elements as $element)
-            {{-- "Three Dots" Separator --}}
-            @if (is_string($element))
-                <div class="ui disabled basic @if ($darkMode) inverted @endif button"
-                     aria-disabled="true">
-                    {{ $element }}
-                </div>
-            @endif
-
-            {{-- Array Of Links --}}
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <div class="ui active @if ($darkMode) inverted @endif button">
-                            {{ $page }}
+        <div class="twelve wide computer only column">
+            <div class="ui compact wrapping spaced buttons">
+                @foreach ($elements as $element)
+                    {{-- "Three Dots" Separator --}}
+                    @if (is_string($element))
+                        <div class="ui disabled basic @if ($darkMode) inverted @endif button"
+                             aria-disabled="true">
+                            {{ $element }}
                         </div>
-                    @else
-                        <a class="ui @if ($darkMode) inverted @else basic @endif button"
-                           href="{{ $url }}">
-                            {{ $page }}
-                        </a>
+                    @endif
+
+                    {{-- Array Of Links --}}
+                    @if (is_array($element))
+                        @foreach ($element as $page => $url)
+                            @if ($page == $paginator->currentPage())
+                                <div class="ui active @if ($darkMode) inverted @endif button">
+                                    {{ $page }}
+                                </div>
+                            @else
+                                <a class="ui @if ($darkMode) inverted @else basic @endif button"
+                                   href="{{ $url }}">
+                                    {{ $page }}
+                                </a>
+                            @endif
+                        @endforeach
                     @endif
                 @endforeach
-            @endif
-        @endforeach
+            </div>
+        </div>
 
         {{-- Next Page Link --}}
-        @if ($paginator->hasMorePages())
-            <a class="ui right floated @if ($darkMode) inverted @else basic @endif button"
-               href="{{ $paginator->nextPageUrl() }}"
-               rel="next">
-                {!! __('pagination.next') !!}
-            </a>
-        @else
-            <div class="ui disabled right floated @if ($darkMode) inverted basic @endif button"
-                 aria-disabled="true">
-                {!! __('pagination.next') !!}
+        <div class="two wide computer four wide tablet eight wide mobile right floated column">
+            <div class="ui compact fluid buttons">
+                @if ($paginator->hasMorePages())
+                    <a class="ui @if ($darkMode) inverted @else basic @endif button"
+                       href="{{ $paginator->nextPageUrl() }}"
+                       rel="next">
+                        {!! __('pagination.next') !!}
+                    </a>
+                @else
+                    <div class="ui disabled @if ($darkMode) inverted basic @endif button"
+                         aria-disabled="true">
+                        {!! __('pagination.next') !!}
+                    </div>
+                @endif
             </div>
-        @endif
+        </div>
+    </div>
+    <div class="ui one column compact grid">
+        <div class="mobile tablet only column">
+            <div class="ui compact wrapping spaced buttons">
+                @foreach ($elements as $element)
+                    {{-- "Three Dots" Separator --}}
+                    @if (is_string($element))
+                        <div class="ui disabled basic @if ($darkMode) inverted @endif button"
+                             aria-disabled="true">
+                            {{ $element }}
+                        </div>
+                    @endif
+
+                    {{-- Array Of Links --}}
+                    @if (is_array($element))
+                        @foreach ($element as $page => $url)
+                            @if ($page == $paginator->currentPage())
+                                <div class="ui active @if ($darkMode) inverted @endif button">
+                                    {{ $page }}
+                                </div>
+                            @else
+                                <a class="ui @if ($darkMode) inverted @else basic @endif button"
+                                   href="{{ $url }}">
+                                    {{ $page }}
+                                </a>
+                            @endif
+                        @endforeach
+                    @endif
+                @endforeach
+            </div>
+        </div>
     </div>
 @endif
