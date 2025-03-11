3506f5f005a4541cbe8cb429d9110c706ae54ab2
ghabriel <rihulfaakbar@gmail.com>
2025-03-11 21:01:23 +0700

    feat(structure): update simple-bootstrap.blade.php responsiveness

--- resources/views/bootstrap/simple-bootstrap.blade.php
+++ resources/views/bootstrap/simple-bootstrap.blade.php
@@ -1,34 +1,38 @@
 @if ($paginator->hasPages())
     @props(['darkMode' => false])
 
-    <div class="d-grid gap-2 d-sm-block clearfix">
+    <div class="row justify-content-between clearfix row-gap-2">
         {{-- Previous Page Link --}}
-        @if ($paginator->onFirstPage())
-            <button class="btn @if ($darkMode) btn-outline-light @else btn-outline-primary @endif float-start"
-                    aria-disabled="true"
-                    disabled>
-                {!! __('pagination.previous') !!}
-            </button>
-        @else
-            <a class="btn @if ($darkMode) btn-outline-light @else btn-outline-primary @endif float-start"
-               href="{{ $paginator->previousPageUrl() }}"
-               role="button">
-                {!! __('pagination.previous') !!}
-            </a>
-        @endif
+        <div class="d-grid col-lg-2 col-sm-3 col-6">
+            @if ($paginator->onFirstPage())
+                <button class="btn @if ($darkMode) btn-outline-light @else btn-outline-primary @endif"
+                        aria-disabled="true"
+                        disabled>
+                    {!! __('pagination.previous') !!}
+                </button>
+            @else
+                <a class="btn @if ($darkMode) btn-outline-light @else btn-outline-primary @endif"
+                   href="{{ $paginator->previousPageUrl() }}"
+                   role="button">
+                    {!! __('pagination.previous') !!}
+                </a>
+            @endif
+        </div>
 
         {{-- Next Page Link --}}
-        @if ($paginator->hasMorePages())
-            <a class="btn @if ($darkMode) btn-outline-light @else btn-outline-primary @endif float-end"
-               href="{{ $paginator->nextPageUrl() }}">
-                {!! __('pagination.next') !!}
-            </a>
-        @else
-            <button class="btn @if ($darkMode) btn-outline-light @else btn-outline-primary @endif float-end"
-                    aria-disabled="true"
-                    disabled>
-                {!! __('pagination.next') !!}
-            </button>
-        @endif
+        <div class="d-grid col-lg-2 col-sm-3 col-6 ms-auto">
+            @if ($paginator->hasMorePages())
+                <a class="btn @if ($darkMode) btn-outline-light @else btn-outline-primary @endif"
+                   href="{{ $paginator->nextPageUrl() }}">
+                    {!! __('pagination.next') !!}
+                </a>
+            @else
+                <button class="btn @if ($darkMode) btn-outline-light @else btn-outline-primary @endif"
+                        aria-disabled="true"
+                        disabled>
+                    {!! __('pagination.next') !!}
+                </button>
+            @endif
+        </div>
     </div>
 @endif
