897dbc7b0504bd9c148b4b17ed923651f1a320b7
ghabriel <rihulfaakbar@gmail.com>
2025-03-11 20:42:28 +0700

    feat(structure): update fomantic-ui responsiveness

--- resources/views/fomanticui/fomantic-ui.blade.php
+++ resources/views/fomanticui/fomantic-ui.blade.php
@@ -1,75 +1,77 @@
 @if ($paginator->hasPages())
     @props(['darkMode' => false])
 
-    <div class="ui three column compact center aligned grid">
-        {{-- Previous Page Link --}}
-        <div class="two wide computer four wide tablet eight wide mobile left floated column">
-            <div class="ui compact fluid buttons">
-                @if ($paginator->onFirstPage())
-                    <div class="ui disabled @if ($darkMode) inverted basic @endif button"
-                         aria-disabled="true">
-                        {!! __('pagination.previous') !!}
-                    </div>
-                @else
-                    <a class="ui @if ($darkMode) inverted @else basic @endif button"
-                       href="{{ $paginator->previousPageUrl() }}"
-                       rel="prev">
-                        {!! __('pagination.previous') !!}
-                    </a>
-                @endif
-            </div>
-        </div>
-
-        {{-- Pagination Elements --}}
-        <div class="twelve wide computer only column">
-            <div class="ui compact wrapping spaced buttons">
-                @foreach ($elements as $element)
-                    {{-- "Three Dots" Separator --}}
-                    @if (is_string($element))
-                        <div class="ui disabled basic @if ($darkMode) inverted @endif button"
+    <div class="ui three column grid">
+        <div class="row">
+            {{-- Previous Page Link --}}
+            <div class="two wide computer four wide tablet eight wide mobile left floated column">
+                <div class="ui compact fluid buttons">
+                    @if ($paginator->onFirstPage())
+                        <div class="ui disabled @if ($darkMode) inverted basic @endif button"
                              aria-disabled="true">
-                            {{ $element }}
+                            {!! __('pagination.previous') !!}
+                        </div>
+                    @else
+                        <a class="ui @if ($darkMode) inverted @else basic @endif button"
+                           href="{{ $paginator->previousPageUrl() }}"
+                           rel="prev">
+                            {!! __('pagination.previous') !!}
+                        </a>
+                    @endif
+                </div>
+            </div>
+
+            {{-- Pagination Elements --}}
+            <div class="twelve wide computer only column center aligned">
+                <div class="ui compact wrapping spaced buttons">
+                    @foreach ($elements as $element)
+                        {{-- "Three Dots" Separator --}}
+                        @if (is_string($element))
+                            <div class="ui disabled basic @if ($darkMode) inverted @endif button"
+                                 aria-disabled="true">
+                                {{ $element }}
+                            </div>
+                        @endif
+
+                        {{-- Array Of Links --}}
+                        @if (is_array($element))
+                            @foreach ($element as $page => $url)
+                                @if ($page == $paginator->currentPage())
+                                    <div class="ui active @if ($darkMode) inverted @endif button">
+                                        {{ $page }}
+                                    </div>
+                                @else
+                                    <a class="ui @if ($darkMode) inverted @else basic @endif button"
+                                       href="{{ $url }}">
+                                        {{ $page }}
+                                    </a>
+                                @endif
+                            @endforeach
+                        @endif
+                    @endforeach
+                </div>
+            </div>
+
+            {{-- Next Page Link --}}
+            <div class="two wide computer four wide tablet eight wide mobile right floated column">
+                <div class="ui compact fluid buttons">
+                    @if ($paginator->hasMorePages())
+                        <a class="ui @if ($darkMode) inverted @else basic @endif button"
+                           href="{{ $paginator->nextPageUrl() }}"
+                           rel="next">
+                            {!! __('pagination.next') !!}
+                        </a>
+                    @else
+                        <div class="ui disabled @if ($darkMode) inverted basic @endif button"
+                             aria-disabled="true">
+                            {!! __('pagination.next') !!}
                         </div>
                     @endif
-
-                    {{-- Array Of Links --}}
-                    @if (is_array($element))
-                        @foreach ($element as $page => $url)
-                            @if ($page == $paginator->currentPage())
-                                <div class="ui active @if ($darkMode) inverted @endif button">
-                                    {{ $page }}
-                                </div>
-                            @else
-                                <a class="ui @if ($darkMode) inverted @else basic @endif button"
-                                   href="{{ $url }}">
-                                    {{ $page }}
-                                </a>
-                            @endif
-                        @endforeach
-                    @endif
-                @endforeach
-            </div>
-        </div>
-
-        {{-- Next Page Link --}}
-        <div class="two wide computer four wide tablet eight wide mobile right floated column">
-            <div class="ui compact fluid buttons">
-                @if ($paginator->hasMorePages())
-                    <a class="ui @if ($darkMode) inverted @else basic @endif button"
-                       href="{{ $paginator->nextPageUrl() }}"
-                       rel="next">
-                        {!! __('pagination.next') !!}
-                    </a>
-                @else
-                    <div class="ui disabled @if ($darkMode) inverted basic @endif button"
-                         aria-disabled="true">
-                        {!! __('pagination.next') !!}
-                    </div>
-                @endif
+                </div>
             </div>
         </div>
     </div>
-    <div class="ui one column compact grid">
+    <div class="ui one column grid">
         <div class="mobile tablet only column">
             <div class="ui compact wrapping spaced buttons">
                 @foreach ($elements as $element)
--- resources/views/fomanticui/simple-fomantic-ui.blade.php
+++ resources/views/fomanticui/simple-fomantic-ui.blade.php
@@ -1,35 +1,38 @@
 @if ($paginator->hasPages())
     @props(['darkMode' => false])
 
-    <div class="ui grid">
-        <div class="column">
-            {{-- Previous Page Link --}}
-            @if ($paginator->onFirstPage())
-                <div class="ui disabled left floated @if ($darkMode) inverted basic @endif compact button"
-                     aria-disabled="true">
-                    {!! __('pagination.previous') !!}
-                </div>
-            @else
-                <a class="ui left floated @if ($darkMode) inverted @else basic @endif compact button"
-                   href="{{ $paginator->previousPageUrl() }}"
-                   rel="prev">
-                    {!! __('pagination.previous') !!}
-                </a>
-            @endif
-
-            {{-- Next Page Link --}}
-            @if ($paginator->hasMorePages())
-                <a class="ui right floated @if ($darkMode) inverted @else basic @endif compact button"
-                   href="{{ $paginator->nextPageUrl() }}"
-                   rel="next">
-                    {!! __('pagination.next') !!}
-                </a>
-            @else
-                <div class="ui disabled right floated @if ($darkMode) inverted basic @endif compact button"
-                     aria-disabled="true">
-                    {!! __('pagination.next') !!}
-                </div>
-            @endif
+    <div class="ui two column compact center aligned grid">
+        <div class="row">
+            <div class="two wide computer four wide tablet eight wide mobile left floated column">
+                {{-- Previous Page Link --}}
+                @if ($paginator->onFirstPage())
+                    <div class="ui disabled @if ($darkMode) inverted basic @endif compact fluid button"
+                         aria-disabled="true">
+                        {!! __('pagination.previous') !!}
+                    </div>
+                @else
+                    <a class="ui @if ($darkMode) inverted @else basic @endif compact fluid button"
+                       href="{{ $paginator->previousPageUrl() }}"
+                       rel="prev">
+                        {!! __('pagination.previous') !!}
+                    </a>
+                @endif
+            </div>
+            <div class="two wide computer four wide tablet eight wide mobile right floated column">
+                {{-- Next Page Link --}}
+                @if ($paginator->hasMorePages())
+                    <a class="ui @if ($darkMode) inverted @else basic @endif compact fluid button"
+                       href="{{ $paginator->nextPageUrl() }}"
+                       rel="next">
+                        {!! __('pagination.next') !!}
+                    </a>
+                @else
+                    <div class="ui disabled @if ($darkMode) inverted basic @endif compact fluid button"
+                         aria-disabled="true">
+                        {!! __('pagination.next') !!}
+                    </div>
+                @endif
+            </div>
         </div>
     </div>
 @endif
