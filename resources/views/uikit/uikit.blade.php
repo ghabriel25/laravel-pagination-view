e9d5d423ce07f397fe83f37faa24411ff0c2fb97
ghabriel <rihulfaakbar@gmail.com>
2025-03-14 23:34:25 +0700

    feat(structure): update uikit.blade.php responsiveness

--- resources/views/uikit/uikit.blade.php
+++ resources/views/uikit/uikit.blade.php
@@ -4,12 +4,12 @@
     <div class="uk-flex uk-flex-between uk-flex-wrap @if ($darkMode) uk-light @endif">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <button class="uk-button uk-button-small uk-button-default uk-width-1-2 uk-width-small@s uk-width-1-5@m uk-width-small@l uk-margin-small-top"
+            <button class="uk-button uk-button-small uk-button-default uk-width-1-2 uk-width-1-4@s uk-width-small@l uk-margin-small-top"
                     disabled>
                 {!! __('pagination.previous') !!}
             </button>
         @else
-            <a class="uk-button uk-button-small uk-button-default uk-width-1-2 uk-width-small@s uk-width-1-5@m uk-width-small@l uk-margin-small-top"
+            <a class="uk-button uk-button-small uk-button-default uk-width-1-2 uk-width-1-4@s uk-width-small@l uk-margin-small-top"
                href="{{ $paginator->previousPageUrl() }}">
                 {!! __('pagination.previous') !!}
             </a>
@@ -17,12 +17,12 @@
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a class="uk-button uk-button-small uk-button-default uk-width-1-2 uk-width-small@s uk-width-1-5@m uk-width-small@l uk-flex-last@l uk-margin-small-top"
+            <a class="uk-button uk-button-small uk-button-default uk-width-1-2 uk-width-1-4@s uk-width-small@l uk-flex-last@l uk-margin-small-top"
                href="{{ $paginator->nextPageUrl() }}">
                 {!! __('pagination.next') !!}
             </a>
         @else
-            <button class="uk-button uk-button-small uk-button-default uk-width-1-2 uk-width-small@s uk-width-1-5@m uk-width-small@l uk-flex-last@l uk-margin-small-top"
+            <button class="uk-button uk-button-small uk-button-default uk-width-1-2 uk-width-1-4@s uk-width-small@l uk-flex-last@l uk-margin-small-top"
                     disabled>
                 {!! __('pagination.next') !!}
             </button>
